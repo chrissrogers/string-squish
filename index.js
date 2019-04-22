42e00a2d647ed6bea35a70c3d1fa2dc05c8b9b2d
Christopher Rogers <chrissrogers@gmail.com>
2019-04-22 15:14:55 -0500

    Version 1.0.3 - Bug fix: ES5-compatible string - Cleans up versioning

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var CONSECUTIVE_SPACES = /\s+/g;
 
 module.exports = function squish (string) {
-  if (typeof string !== 'string') throw `Invalid 'string'. Expected String, got ${typeof string}`;
+  if (typeof string !== 'string') throw 'Invalid \'string\'. Expected String, got ' + typeof string;
   return string.trim().replace(CONSECUTIVE_SPACES, ' ');
 }
